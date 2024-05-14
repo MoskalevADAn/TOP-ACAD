6fa1a8fa8b1b6635227b99a115543fc3a4679d4f
MoskalevADAn <moskalevanton900@gmail.com>
2024-05-14 21:21:40 +1000

    Я крутой я сделл первый репозиторий

---
+++ lesson_1/script.js
@@ -0,0 +1,75 @@
+// alert("Привет Мир!")
+
+
+// Объявление переменной
+
+// let message;
+
+// message = 2;
+
+// console.log(message)
+
+// let one = 1;
+// let two = 2;
+// let sum = one + two
+
+// console.log(one)
+// console.log(two)
+// console.log(sum)
+
+
+// let basket = 10
+
+// let basket2 = basket
+// basket2 = 1
+
+// basket2 = 3
+
+// Константа
+
+// const COLOR_RED = "#HEX"
+// COLOR_RED = 9
+
+// let reversetring
+
+// Типы данных
+
+// let message = "Привет мир"
+// let message2 = 'Привет мир'
+// let message3 = "Привет 'мир'"
+// let message4 = 'Привет "мир"'
+
+
+
+// console.log(message)
+// console.log(message2)
+// console.log(message3)
+// console.log(message4)
+
+
+// let name = prompt("Как тебя зовуь?")
+// let message = `Привет, ${name}`
+
+// alert(message)
+let number1 = 10
+let number2 = 10.30
+
+// string - строка
+// number - число
+
+// bigInt
+
+let bigInt2 = 123213123123123213213213n
+
+// boolean - правда 
+
+// // + - * ** / < > || &&
+
+// let a = 1
+// let b = false
+
+// let bool1 = 1 > 2
+
+// let age = null;
+
+// let age2 = undefined
